ecf2be14c6346dd11085615f6932517147f79b48
Shubham Sanjiv Naik <naikshub8412@gmail.com>
2020-01-07 20:41:48 +0530

    Updated App

--- app/src/main/java/com/example/SchoolManagement/ParentMainIndexActivity.java
+++ app/src/main/java/com/example/SchoolManagement/ParentMainIndexActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
