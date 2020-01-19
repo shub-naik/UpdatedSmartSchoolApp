5f394eac49f655b3bf14d9973367b20893fc20a5
maheshgadri <59181365+maheshgadri@users.noreply.github.com>
2020-01-19 18:02:06 +0530

    Teacher Management UI done

--- app/src/main/java/com/pragyatitsolutions/SchoolManagement/DeleteTeacher.java
+++ app/src/main/java/com/pragyatitsolutions/SchoolManagement/DeleteTeacher.java
@@ -33,6 +33,7 @@ public class DeleteTeacher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_teacher);
+        getSupportActionBar().setTitle("Delete Teacher");
 
         alteachers = findViewById(R.id.AllTeachersData);
 
